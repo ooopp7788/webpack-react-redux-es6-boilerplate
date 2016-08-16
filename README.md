75d6d73e04fd9a3ccea0648ba71feec1c1a5359d
chachaxw <867571123@qq.com>
2016-08-16 18:43:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Getting start
+
+  1. Clone repo:
+  ```
+  $ git clone git@github.com:chachaxw/webpack-react-redux-es6-boilerplate.git
+  // or
+  $ git clone https://github.com/chachaxw/webpack-react-redux-es6-boilerplate.git
+  ```
+
+  2. Install dependencies
+  ```
+  $ npm i
+  ```
+
+  3. Debug
+  ```
+  $ npm start
+  ```
+  then access browser: `http://localhost:3000`
---
+++ build/webpack.dev.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config.js');
+var utils = require('./utils');
+
+var PORT = 3000;
+var HOST = utils.getIP();
+var args = process.argv;
+var hot = args.indexOf('--hot') > -1;
+var deploy = args.indexOf('--deploy') > -1;
+
+// 本地环境静态资源路径
+var localPublicPath = 'http://' + HOST + ':' + PORT + '/';
+
+config.output.publicPath = localPublicPath;
+config.entry.app.unshift('webpack-dev-server/client?' + localPublicPath);
+
+new WebpackDevServer(webpack(config), {
+  hot: hot,
+  inline: true,
+  compress: true,
+  stats: {
+    chunks: false,
+    children: false,
+    colors: true
+  },
+  // Set this as true if you want to access dev server from arbitrary url.
+  // This is handy if you are using a html5 router.
+  historyApiFallback: true,
+}).listen(PORT, HOST, function() {
+  console.log(localPublicPath);
+});
