6cf9173c44464ce3cc3cd63bc56997c3c0e6f59f
Wei Zhou <867571123@qq.com>
2017-01-19 11:25:12 +0800

    添加react组件

--- src/components/Index/IndexPage.js
+++ src/components/Index/IndexPage.js
@@ -13,20 +13,40 @@ import * as counterActions from 'actions/counterActions';
 )
 export default class IndexPage extends Component {
 
+  state = {
+    dragging: false,
+    // quadratic bezier control point
+    c: { x: 160, y: 160 },
+    // record drag start point
+    start: { x: 0, y: 0 }
+  }
+
   render() {
     const { counter, inc, dec } = this.props;
-
-    // console.log(inc, dec);
+    const d = 'M0,0 L320,0 320,160' + 'Q' + this.state.c.x + ',' + this.state.c.y + ' 0,160';
 
     return (
       <div>
-        <h3>Webpack-React-Redux-ES6-Boilerplate <br/> Dev Environment</h3>
-        <div className="redux-counter">
-          <h2>Redux Counter</h2>
-          <button onClick={() => dec()}>-</button>
-          <span>{counter}</span>
-          <button onClick={() => inc()}>+</button>
-          <p>打开控制台,可查看counter变化日志</p>
+        <h3 className="title">Webpack-React-Redux-ES6-Boilerplate <br/> Dev Environment</h3>
+        <div className="draggable-header-view">
+          <svg className="bg" width="320" height="560">
+            <path d={{d}} style={{fill: '#3F51B5'}}></path>
+          </svg>
+          <div className="header">
+            <h1>Elastic Draggable SVG Header</h1>
+          </div>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+              Aenean commodo ligula eget dolor. Aenean massa.
+              Cum sociis natoque penatibus et magnis dis parturient montes,
+              nascetur ridiculus mus. Donec quam felis, ultricies nec,
+              pellentesque eu, pretium quis, sem. Nulla consequat
+              massa quis enim. Donec pede justo, fringilla vel,
+              aliquet nec, vulputate eget, arcu. In enim justo,
+              rhoncus ut, imperdiet a, venenatis vitae, justo.
+            </p>
+          </div>
         </div>
       </div>
     )
